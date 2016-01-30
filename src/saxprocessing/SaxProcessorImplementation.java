08e9bf1391fa19ddeb79cc6bce9879222ab588f7
Junaid Pasha <junaidpasha@Junaid-Pashas-MacBook-Pro.local>
2016-01-30 13:39:27 -0600

    Sax Processor

---
+++ src/saxprocessing/SaxProcessorImplementation.java
@@ -0,0 +1,9 @@
+package saxprocessing;
+
+public class SaxProcessorImplementation {
+	 public static void main(String args[]){
+		   SaxProcessor readXml = new SaxProcessor();
+		   readXml.getXml();
+		  }
+
+}
